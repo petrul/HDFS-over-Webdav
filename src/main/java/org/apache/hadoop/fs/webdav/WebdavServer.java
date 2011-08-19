4667deef286a026e18bdf8e44e44523e0a8b79b1
Max Gensthaler <max@gensthaler.de>
2011-08-20 00:28:32 +0200

    detailing usage info with default values

--- src/main/java/org/apache/hadoop/fs/webdav/WebdavServer.java
+++ src/main/java/org/apache/hadoop/fs/webdav/WebdavServer.java
@@ -29,10 +29,12 @@ import org.mortbay.xml.XmlConfiguration;
 import java.io.FileInputStream;
 
 public class WebdavServer {
+	private static final Log LOG = LogFactory.getLog(WebdavServer.class);
 
-    private static final Log LOG = LogFactory.getLog(WebdavServer.class);
-
-    public static String WEB_APP_CONTEXT = "webAppContext";
+    public static final String WEB_APP_CONTEXT = "webAppContext";
+    private static final String DEFAULT_FS_NAME = "hdfs://127.0.0.1:8020";
+    private static final String DEFAULT_LISTEN_ADDRESS = "0.0.0.0";
+    private static final String DEFAULT_BIND_PORT = "19800";
 
     private Server webServer;
 
@@ -40,7 +42,7 @@ public class WebdavServer {
         LOG.info("Initializing webdav server");
 
         webServer = new Server();
-        XmlConfiguration configuration = new XmlConfiguration(new FileInputStream("conf/jetty.xml")); 
+        XmlConfiguration configuration = new XmlConfiguration(new FileInputStream("conf/jetty.xml"));
         configuration.configure(webServer);
 
         Connector connector=new SelectChannelConnector();
@@ -57,10 +59,7 @@ public class WebdavServer {
         String usage = "WebdavServer";
         String header = "Run a webdav interface to a hadoop filesystem.";
         Options options = new Options();
-        options.addOption("l", "listen", true, "address to listen to");
-        options.addOption("p", "port", true, "port to bind to");
-        options.addOption("n", "fs", true, "value for fs.default.name (eg. namenode:port)");
-        options.addOption("h", "help", false, "print usage information");
+        options = buildGeneralOptions(options);
         CommandLineParser parser = new GnuParser();
         CommandLine cmd = parser.parse(options, args);
         if (cmd.hasOption("h")) {
@@ -68,20 +67,36 @@ public class WebdavServer {
             formatter.printHelp(usage, header, options, "");
             return;
         }
-        int port = Integer.parseInt(cmd.getOptionValue("port", "19800"));
+        int port = Integer.parseInt(cmd.getOptionValue("port", DEFAULT_BIND_PORT));
 
         // we use this cheesy way of passing the configuration to the WebdavServlet because
         //  jetty-5 doesn't have a way to send it in the WebdavServlet constructor
         Configuration config = new Configuration();
-        String fsDefaultName = cmd.getOptionValue("fs", null);
+        String fsDefaultName = cmd.getOptionValue("fs", DEFAULT_FS_NAME);
         if (fsDefaultName != null) {
             config.set("fs.default.name", fsDefaultName);
         }
         WebdavServlet.setConf(config);
 
-        WebdavServer server = new WebdavServer(cmd.getOptionValue("l", "0.0.0.0"), port);
+        WebdavServer server = new WebdavServer(cmd.getOptionValue("l", DEFAULT_LISTEN_ADDRESS), port);
         LOG.info("Starting webdav server");
         server.start();
     }
 
+	private static Options buildGeneralOptions(Options opts) {
+		Option listenOpt = new Option("l", "listen", true, "address to listen to, default: " + DEFAULT_LISTEN_ADDRESS);
+        listenOpt.setArgName("address");
+		opts.addOption(listenOpt);
+
+		Option portOpt = new Option("p", "port", true, "port to bind to, default: " + DEFAULT_BIND_PORT);
+        portOpt.setArgName("port");
+		opts.addOption(portOpt);
+
+		Option fsOpt = new Option("n", "fs", true, "value for fs.default.name, default: " + DEFAULT_FS_NAME);
+        fsOpt.setArgName("uri");
+		opts.addOption(fsOpt);
+
+		opts.addOption("h", "help", false, "print usage information");
+        return opts;
+	}
 }
